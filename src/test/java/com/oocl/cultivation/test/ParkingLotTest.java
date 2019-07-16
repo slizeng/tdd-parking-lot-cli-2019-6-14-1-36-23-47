0cceae1fad869079815553412e816d618928ed09
Chaowei Zeng <sli616@163.com>
2019-07-16 15:58:29 +0800

    add unit test to verify default parking log capacity as AC requires.

--- src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -9,6 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     @Test
+    void should_return_10_when_getAvailableParkingPosition_given_a_default_parking_lot() {
+        ParkingLot parkingLot = new ParkingLot();
+
+        int parkingPosition = parkingLot.getAvailableParkingPosition();
+
+        assertEquals(10, parkingPosition);
+    }
+
+    @Test
     void should_return_a_number_when_getAvailableParkingPosition_given_a_parking_lot_with_capacity() {
         ParkingLot parkingLot = new ParkingLot(1);
 
@@ -26,7 +35,6 @@ public class ParkingLotTest {
 
         assertNotNull(parkingTicket);
     }
-
 
     @Test
     void should_available_parking_position_reduces_one_when_park_a_car_into_parkingLot_given_a_parkingLot_has_capacity() {
